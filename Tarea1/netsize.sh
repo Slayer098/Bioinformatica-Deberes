4f598ea4dc52a7a183fa9eef95bdb31556d41454
Anderson <95886441+Slayer098@users.noreply.github.com>
2021-12-09 20:42:12 -0600

    Update netsize.sh
    modifique el translación

--- Tarea1/netsize.sh
+++ Tarea1/netsize.sh
@@ -1,20 +1,20 @@
 " Deber Bioformatica 1.10.3 "
-echo " Saavedra and Stouffer (2013) studied several plant–pollinator networks.
+" Saavedra and Stouffer (2013) studied several plant–pollinator networks.
 
 These can be represented as rectangular matrices where the rows are polli-
 nators, the columns plants, a 0 indicates the absence and 1 the presence of an
 
 interaction between the plant and the pollinator. "
-echo " Usaremos el archivo n3.txt "
+" Usaremos el archivo n3.txt "
 cat n3.txt
-echo " Voy a imprimir cat../Saavedra2013/n3.txt | wc -l "
-echo " cat imprime contenido de los archivos ( cat file1 file2 file3) "
-echo " wc -l Cuenta solo líneas ( sort w, sort c) "
-echo " n de filas "
+" Voy a imprimir cat../Saavedra2013/n3.txt | wc -l "
+" cat imprime contenido de los archivos ( cat file1 file2 file3) "
+" wc -l Cuenta solo líneas ( sort w, sort c) "
+" n de filas "
 cat ../Saavedra2013/n3.txt | wc -l
-echo " n de columnas "
-echo " head -n1 ../Saavedra2013/n3.txt | tr -d " " | tr -d "\ n" | wc -c "
-echo " head muestra las filas(lo que pongamos) "
-echo " tr translasion de caracteres "
+" n de columnas "
+" head -n1 ../Saavedra2013/n3.txt | tr -d " " | tr -d "\ n" | wc -c "
+" head muestra las filas(lo que pongamos) "
+" tr translasion de caracteres de un archivo "
 head -n1 ../Saavedra2013/n3.txt | tr -d " " | tr -d "\ n" | wc -c
 
