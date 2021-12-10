5baec27728a6d332c4835dabefddf90523de0608
Anderson <95886441+Slayer098@users.noreply.github.com>
2021-12-09 20:49:59 -0600

    Create netsize_all.sh
    Cree la nueva carpeta del segundo deber

---
+++ Tarea1/netsize_all.sh
@@ -0,0 +1,7 @@
+" Use of For "
+" Write a script that prints the numbers of rows and columns for each network "
+" for file in $(ls *.txt); do wc -l $file;  head -n1 $file | grep -o " " | wc -l; done "
+" for permite repetir una tarea con ligeras variaciones, generalmente si son tareas "
+" greap--> útiles disponibles. Este busca un patrón que definamos en un archivo de texto "
+for file in $(ls *.txt); do wc -l $file;  head -n1 $file | grep -o " " | wc -l; done
+" Anderson Randy Yumbo Licuy "
